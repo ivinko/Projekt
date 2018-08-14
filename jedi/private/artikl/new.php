b05672317739709b1f9c42b57db064111123d773
ivinko <38477708+ivinko@users.noreply.github.com>
2018-08-14 13:38:23 +0200

    artikl

---
+++ jedi/private/artikl/new.php
@@ -0,0 +1,56 @@
+<?php 
+include_once "../../config.php";
+
+if(!isset($_SESSION["o"])){
+  header('location:'.$putanjaAPP.'logout.php');
+} 
+
+if(isset($_POST["dodaj"])){
+    $izraz = $veza->prepare("insert into artikl(naziv, kategorija, opis, cijena) values
+                            (:naziv, :kategorija, :opis, :cijena)");
+    unset($_POST["dodaj"]);
+    $izraz->execute($_POST); 
+    header("location: index.php"); 
+}
+?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <?php include_once "../../add/head.php" ?>
+  </head>
+  <body>
+    <div class="grid-container">
+      
+    <?php include_once "../../add/heading.php" ?>
+
+    <?php include_once "../../add/menu.php" ?>
+
+   
+   <div class="grid-x" style="justify-content:center;">
+        <div class="cell medium-4 large-3">
+                <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+                            <h4 class="text-center">Novi artikl</h4>
+                            <label>Naziv
+                                <input type="text"  name="naziv">
+                            </label>
+                            <label>Kategorija
+                                <input type="text"  name="kategorija" >
+                            </label>
+                            <label>Opis
+                                <input type="text"  name="opis">
+                            </label>
+                            <label>Cijena
+                                <input type="number"  name="cijena">
+                            </label>
+                            <br>
+                            <input type="submit" name="dodaj" class="button" value="Dodaj"></input>
+                            <a href="index.php" class="alert button">Cancel</a>
+                </form>
+        </div>
+    </div>
+
+    <?php include_once "../../add/podnozje.php" ?>
+
+    <?php include_once "../../add/script.php" ?>
+  </body>
+</html>
---
+++ jedi/private/artikl/rewrite.php
@@ -0,0 +1,65 @@
+<?php include_once "../../config.php"; 
+
+if(!isset($_SESSION["o"])){
+    header('location:'.$putanjaAPP.'logout.php');
+}
+if(!isset($_GET["sifra"]) && !isset($_POST["uredi"])){
+    header('location:'.$putanjaAPP.'logout.php');
+}
+
+
+  if(isset($_POST["uredi"])){
+    $izraz = $veza->prepare("update artikl set naziv=:naziv, kategorija=:kategorija, opis=:opis, cijena=:cijena where sifra=:sifra;"); 
+    unset($_POST["uredi"]);
+    $izraz->execute($_POST);
+    header("location: index.php"); 
+}else{
+    $izraz = $veza->prepare("select * from artikl where sifra=:sifra");
+    $izraz->execute($_GET);
+    $rezultat = $izraz->fetch(PDO::FETCH_OBJ);  
+}
+
+?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <?php include_once "../../add/head.php" ?>
+  </head>
+  <body>
+    <div class="grid-container">
+      
+    <?php include_once "../../add/heading.php" ?>
+
+    <?php include_once "../../add/menu.php" ?>
+
+    <form class="callout text-center" action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
+  
+  <div class="floated-label-wrapper">
+    <label for="naziv">Naziv</label>
+    <input value="<?php echo $rezultat->naziv ?>" autocomplete="off" type="text" id="naziv" name="naziv" >
+  </div>
+  <div class="floated-label-wrapper">
+    <label for="kategorija">Kategorija</label>
+    <input value="<?php echo $rezultat->kategorija ?>" autocomplete="off" type="text" id="kategorija" name="kategorija" >
+  </div>
+  <div class="floated-label-wrapper">
+    <label for="opis">Opis</label>
+    <input value="<?php echo $rezultat->opis ?>" autocomplete="off" type="text" id="opis" name="opis" >
+  </div>
+  <div class="floated-label-wrapper">
+    <label for="cijena">Cijena</label>
+    <input  value="<?php echo $rezultat->cijena ?>" autocomplete="off" type="number" min="1" max="10000" id="cijena" name="cijena" >
+  </div>
+  <br>
+            <input type="hidden" name="sifra" value="<?php echo $rezultat->sifra ?>" />
+            <input type="submit" name="uredi" class="button" value="Uredi"></input>
+            <a href="index.php" class="alert button">Cancel</a>
+  
+</form>
+
+
+    <?php include_once "../../add/podnozje.php" ?>
+
+    <?php include_once "../../add/script.php" ?>
+  </body>
+</html>
